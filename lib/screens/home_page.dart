dee5b7baab68fd587467f1f8363d5baa9d1cafad
Visual_Archive_1 <maharjan.saroj93@gmail.com>
2021-04-03 13:17:42 +0545

    Check Box added

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -25,6 +25,7 @@ class _HomeScreenState extends State<HomeScreen> {
       _tasks.add(Task.fromMap(json.decode(d)));
     }
     print(_tasks);
+    setState(() {});
   }
 
   void saveDate() async {
@@ -43,7 +44,9 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
+
     _getTask();
+
     _taskController = TextEditingController();
   }
 
@@ -63,19 +66,48 @@ class _HomeScreenState extends State<HomeScreen> {
           ? Center(
               child: Text("No Tasks Added Yet!"),
             )
-          : Column(
-              children: _tasks
-                  .map(
-                    (e) => Container(
-                      height: 70.0,
-                      width: MediaQuery.of(context).size.width,
-                      child: Text(
-                        e.task,
-                        style: TextStyle(color: Colors.yellow),
+          : SingleChildScrollView(
+              child: Column(
+                children: _tasks
+                    .map(
+                      (e) => Container(
+                        height: 70.0,
+                        width: MediaQuery.of(context).size.width,
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 10.0,
+                          vertical: 5.0,
+                        ),
+                        padding: const EdgeInsets.only(left: 10.0),
+                        alignment: Alignment.centerLeft,
+                        decoration: BoxDecoration(
+                          color: Colors.blueAccent,
+                          borderRadius: BorderRadius.circular(5.0),
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 0.5,
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              e.task,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Checkbox(
+                              value: false,
+                              onChanged: (value) {
+                                print(value);
+                              },
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  )
-                  .toList(),
+                    )
+                    .toList(),
+              ),
             ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add, color: Colors.white),
